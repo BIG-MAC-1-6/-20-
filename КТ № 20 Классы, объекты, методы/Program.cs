826f8ced7c3c9fe54dd342544d1b3edda19374fc
Nikei <Nikei@DESKTOP-NH3O0C1>
2024-12-18 11:55:03 +1000

    Добавьте файлы проекта.

---
+++ КТ № 20 Классы, объекты, методы/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КТ___20_Классы__объекты__методы
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student student1 = new Student("Иван Петров", 18, "ИВТ-21");
+            Student student2 = new Student("Мария Иванова", 19, "ЭК-22");
+            Student student3 = new Student("Сергей Сидоров", 20, "ФИТ-21");
+
+            Console.WriteLine(student1);
+            Console.WriteLine(student2);
+            Console.WriteLine(student3);
+
+            Console.ReadKey(); // Задержит консоль, чтобы вы смогли увидеть вывод
+        }
+    }
+}
---
+++ КТ № 20 Классы, объекты, методы/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КТ___20_Классы__объекты__методы
+{
+    public class Student
+    {
+        public string Name;
+        public int Age;
+        public string Group;
+
+        public Student(string name, int age, string group)
+        {
+            Name = name;
+            Age = age;
+            Group = group;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Group: {Group}";
+        }
+    }
+}
